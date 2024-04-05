c2cf67f12fe9e893aa174cf61faa53f47e8a7c63
Mariaigutova <145985662+Mariaigutova@users.noreply.github.com>
2024-04-05 22:59:21 +0300

    Add files via upload

---
+++ class.cpp
@@ -0,0 +1,21 @@
+﻿#include "PFC.h"
+#include "Location.h"
+#include "Snack.h"
+#include "SnackSlot.h"
+#include "VendingMachine.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    Snack s;
+    SnackSlot ss;
+    VendingMachine m;
+    cout << s << endl;
+    cout << m;
+    int grade_for_laboratory;
+    cout << "Grade: ";
+    cin >> grade_for_laboratory;
+    ss.grade_for_laboratory_work(grade_for_laboratory);
+    return 0;
+    
+}
